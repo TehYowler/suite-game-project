8a17d7e00d96c0c43cbaf00cf3d40ecb3fd0c1ca
TehYowler <yowler@nmsu.edu>
2024-11-07 00:12:16 +0000

    Update Platform.java
    Quite a bit has changed with the platforms. For one, only one lambda/interface function is now used for it, called "AnyLambda". Setting the oscillation type is much easier, since you can add it while defining the platforms in the scene in the Main.java class.
    As well as that, collision is added (handled through the player class), and within the Platform class, the part of code with "moveBind" allow the player to move with the platform movement.

--- Project/core/src/main/java/com/rat/gamer/Platform.java
+++ Project/core/src/main/java/com/rat/gamer/Platform.java
@@ -6,10 +6,13 @@ public class Platform extends GameplayObject {
 
     private float oscillationProgress = 0;
     private float oscillationStart = 0;
-    public OscillateTick oscillateType;
+    public AnyLambda oscillateType;
+    public Player moveBind = null;
 
-    public final ObjectOscillate backAndForth = (float x1, float y1, float x2, float y2, float speedInitial, float speedOscillate) -> {
-        return () -> {
+    public Platform oscillateFromTo(float x1, float y1, float x2, float y2, float speedInitial, float speedOscillate) {
+        oscillationStart = 0;
+        oscillationProgress = 0;
+        this.oscillateType = () -> {
             oscillationStart += speedInitial / 400f;
             oscillationProgress += speedOscillate / 200f;
 
@@ -22,17 +25,48 @@ public class Platform extends GameplayObject {
             this.x = this.x * ( 1 - oscillationStart ) + ( x2 * oscillateValue + x1 * ( 1 - oscillateValue ) ) * oscillationStart;
             this.y = this.y * ( 1 - oscillationStart ) + ( y2 * oscillateValue + y1 * ( 1 - oscillateValue ) ) * oscillationStart;
         };
-    };
+        return this;
+    }
+
+    public Platform oscillateRound(float x, float y, float radius, float speedInitial, float speedOscillate) {
+        oscillationStart = 0;
+        oscillationProgress = 0;
+        this.oscillateType = () -> {
+            oscillationStart += speedInitial / 400f;
+            oscillationProgress += speedOscillate / 200f;
+
+            oscillationStart = Math.clamp(oscillationStart, 0,1);
+            oscillationProgress %= 2;
+
+            float xTo = x + (float)Math.cos(oscillationProgress * Math.PI) * radius * 50;
+            float yTo = y + (float)Math.sin(oscillationProgress * Math.PI) * radius * 50;
+
+            float previousX = this.x;
+            float previousY = this.y;
+
+            this.x = this.x * ( 1 - oscillationStart ) + ( xTo ) * oscillationStart;
+            this.y = this.y * ( 1 - oscillationStart ) + ( yTo ) * oscillationStart;
+        };
+        return this;
+    }
 
     public Platform(float x, float y, Texture image, int width, int height) {
         super(x, y, image, width, height, false,0);
         oscillateType = null;
     }
 
-    public void tick() {
+    public void tick(Scene scene) {
         if(oscillateType != null) {
             //oscillateType(1, 2, 3, 3);
+            float previousX = x;
+            float previousY = y;
             oscillateType.run();
+
+            if(moveBind != null) {
+                moveBind.x += (this.x - previousX);
+                moveBind.y += (this.y - previousY);
+                this.moveBind = null;
+            }
         }
     }
 
