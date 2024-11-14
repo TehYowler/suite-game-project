14109ab6e6e28b4aedf62e2d678a1f2f78c35411
TehYowler <yowler@nmsu.edu>
2024-11-14 00:13:03 +0000

    Update Scene.java
    Added arraylists for each added object type. Made the constructor take if there would be a floor or ceiling, and the main class (to help with operating on the level number). Also added a restart state, death state, and a next state, all to handle level logic.

--- Project/core/src/main/java/com/rat/gamer/Scene.java
+++ Project/core/src/main/java/com/rat/gamer/Scene.java
@@ -1,6 +1,8 @@
 package com.rat.gamer;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import java.util.ArrayList;
@@ -10,15 +12,36 @@ public class Scene {
 
     //A scene object defines everything in a certain level, or 'scene".
 
+    private float frame = 0;
+
     public ArrayList<GameplayObject> objectsGeneral = new ArrayList<GameplayObject>();
     public ArrayList<Player> objectsPlayer = new ArrayList<Player>();
     public ArrayList<Platform> objectsPlatform = new ArrayList<Platform>();
     public ArrayList<GravityChange> objectsGravity = new ArrayList<GravityChange>();
-
-    private int orderID = 0;
+    public ArrayList<Flag> objectsFlag = new ArrayList<Flag>();
+    public ArrayList<JumpRefill> objectsRefill = new ArrayList<JumpRefill>();
+    public ArrayList<LevelExit> objectsExit = new ArrayList<LevelExit>();
     //It has an ArrayList for each type of object.
     //EVEN THOUGH "objectsGeneral" ACCEPTS ALL TYPES OF "GameplayObject", CHECKS SUCH AS COLLISION CHECKS WILL NOT BE
     //PERFORMED ON OBJECTS IN IT. ONLY USE IT FOR VISUALS, NOT GAME LOGIC, SUCH AS PLAYERS OR PLATFORMS.
+
+    private int orderID = 0;
+    //Specifies default object order.
+
+    private Main main;
+    //A reference back to the main class using this object.
+
+    public final boolean hasFloor;
+    public final boolean hasCeiling;
+    public Scene(boolean hasFloor, boolean hasCeiling, Main main) {
+        this.hasFloor = hasFloor;
+        this.hasCeiling = hasCeiling;
+        this.main = main;
+    }
+
+    public boolean dead = false;
+    public boolean restart = false;
+    public boolean next = false;
 
     public ArrayList<GameplayObject> allObjects() {
         ArrayList<GameplayObject> allObjects = new ArrayList<GameplayObject>();
@@ -26,6 +49,9 @@ public class Scene {
         allObjects.addAll(objectsPlayer);
         allObjects.addAll(objectsPlatform);
         allObjects.addAll(objectsGravity);
+        allObjects.addAll(objectsFlag);
+        allObjects.addAll(objectsRefill);
+        allObjects.addAll(objectsExit);
         allObjects.sort(Comparator.comparingInt(a -> a.order));
         return allObjects;
     }
@@ -35,6 +61,25 @@ public class Scene {
         for( GameplayObject x : allObjects() ) {
             x.tick(this);
         }
+        if(main.levelNumber == 0 && Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+            main.swapLevel(1);
+        }
+        else if(main.levelNumber < 0 && Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+            main.swapLevel(Math.abs(main.levelNumber));
+        }
+        if(this.next) {
+            main.swapLevel(main.levelNumber + 1);
+        }
+        else if(this.restart) {
+            main.swapLevel(Math.abs(main.levelNumber));
+        }
+        else if(this.dead) {
+            main.swapLevel(-Math.abs(main.levelNumber));
+        }
+        frame++;
+    }
+    public float getFrame() {
+        return frame;
     }
     public void draw(SpriteBatch batch) { //Ticks all objects in the scene forward.
         for( GameplayObject x : allObjects() ) {
@@ -68,6 +113,27 @@ public class Scene {
         for(GravityChange x : objectAdd) {
             x.order = orderID; orderID++;
             objectsGravity.add(x);
+        }
+        return objectAdd;
+    }
+    public LevelExit[] addExit(LevelExit... objectAdd) {
+        for(LevelExit x : objectAdd) {
+            x.order = orderID; orderID++;
+            objectsExit.add(x);
+        }
+        return objectAdd;
+    }
+    public Flag[] addFlag(Flag... objectAdd) {
+        for(Flag x : objectAdd) {
+            x.order = orderID; orderID++;
+            objectsFlag.add(x);
+        }
+        return objectAdd;
+    }
+    public JumpRefill[] addRefill(JumpRefill... objectAdd) {
+        for(JumpRefill x : objectAdd) {
+            x.order = orderID; orderID++;
+            objectsRefill.add(x);
         }
         return objectAdd;
     }
