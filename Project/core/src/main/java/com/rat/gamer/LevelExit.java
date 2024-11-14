1b6d3703d01768e2e59de9d3580caf66f886b4de
TehYowler <yowler@nmsu.edu>
2024-11-14 00:09:32 +0000

    Create LevelExit.java

---
+++ Project/core/src/main/java/com/rat/gamer/LevelExit.java
@@ -0,0 +1,9 @@
+package com.rat.gamer;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class LevelExit extends GameplayObject {
+    public LevelExit(float x, float y, Texture image, int width, int height) {
+        super(x, y, image, width, height);
+    }
+}
