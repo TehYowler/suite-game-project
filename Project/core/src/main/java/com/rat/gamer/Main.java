5e0f6c8b8f52e7f23c9999fbf744bb2dcb13c18d
TehYowler <yowler@nmsu.edu>
2024-11-14 00:18:32 +0000

    Update Main.java
    Added all level logic and a few test levels. For debugging, go to the "create" method and swap out the value in "swapLevel" to 999 for the debug level. Levels are now handled in an if-else statement followed by a switch statement. If the level number is negative, that means to display the game over stage, and the player will restart at the absolute value of the level number. (E.g. if a player fails at level 5, they will be transported to game over at level -5, and when restarting, it will set them to the absolute value of the level, being level 5 again.) Otherwise, if it is positive, form the level. Level 0 is the starting level. Currently we have only tutorial levels, but that is to change.

--- Project/core/src/main/java/com/rat/gamer/Main.java
+++ Project/core/src/main/java/com/rat/gamer/Main.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
@@ -15,41 +16,216 @@ public class Main extends ApplicationAdapter {
     //    private Texture texture;
 //    private TextureRegion region;
 //    private Sprite sprite;
-    private double time = 0;
 
     private Scene currentLevel;
+    public int levelNumber;
+
+    public void addBasic() {
+        Texture background = new Texture("hotel.png");
+        GameplayObject backgroundShow = currentLevel.addGeneral(
+            new Platform(0, 0, background, 1920, 1080)
+        )[0];
+        backgroundShow.tintBlue = 0.8f;
+        backgroundShow.tintRed = 0.6f;
+        backgroundShow.tintGreen = 0.6f;
+    }
+
+
+    public void swapLevel(int levelNumTo) {
+        levelNumber = levelNumTo;
+        if(levelNumber < 0) { //Gameover
+            currentLevel = new Scene(true, true, this);
+            Texture gameOver = new Texture("gameover.png");
+            GameplayObject screen = currentLevel.addGeneral(
+                new Platform(0, 0, gameOver, 1920, 1080)
+            )[0];
+            //Gameover screen
+        }
+        else {
+            switch (levelNumber) {
+                case 0: { //Starting screen
+                    currentLevel = new Scene(true, true, this);
+                    Texture welcome = new Texture("welcome.png");
+                    currentLevel.addGeneral(
+                        new Platform(0, 0, welcome, 1920, 1080)
+                    );
+                    break;
+                }
+
+
+                case 999: {
+                    currentLevel = new Scene(true, false, this);
+                    Texture placeholder = new Texture("cat.png");
+                    Texture refill = new Texture("spring.png");
+                    Texture gravity = new Texture("gravityChange.png");
+                    Texture flagBase = new Texture("flagRed.png");
+
+                    addBasic();
+
+                    currentLevel.addPlayer(
+                        new Player(0, 0, placeholder, 100, 100)
+                    );
+                    //Adds to the scene's ArrayList of player objects.
+
+                    currentLevel.addPlatform(
+                        new Platform(0, 600, placeholder, 150, 10).oscillateFromTo(-400,0,400f,70, 0.2f, 1),
+                        new Platform(0, 600, placeholder, 150, 50).oscillateRound(-400,0,1,2,4),
+                        new Platform(250, -240, placeholder, 450, 200)
+                    );
+                    //Adds to the scene's ArrayList of platform objects.
+                    //One oscillates back and forth between two points, while the other revolves in a circle around a point.
+                    //One is just static.
+
+                    currentLevel.addGravity(
+                        new GravityChange(100,200,gravity,90,90),
+                        new GravityChange(-100,200,gravity,90,90)
+                    );
+
+                    currentLevel.addRefill(
+                        new JumpRefill(500,0,refill,100,100),
+                        new JumpRefill(600,0,refill,100,100),
+                        new JumpRefill(700,0,refill,100,100),
+                        new JumpRefill(400,0,refill,100,100)
+                    );
+
+                    currentLevel.addFlag(
+                        new Flag(200,0,flagBase,90,125),
+                        new Flag(-200,0,flagBase,90,125)
+                    );
+
+                    currentLevel.addExit(
+                        new LevelExit(0,-450,placeholder,75,75)
+                    );
+
+                    break;
+                }
+                case 1: {
+                    currentLevel = new Scene(true, true, this);
+                    Texture placeholder = new Texture("cat.png");
+                    Texture instruction = new Texture("exitPlaceholder.png");
+
+                    addBasic();
+
+                    currentLevel.addGeneral(
+                        new Platform(220, 0, instruction, 800, 400)
+                    );
+
+                    Player player = currentLevel.addPlayer(
+                        new Player(-600, -500, placeholder, 400, 400)
+                    )[0];
+                    player.jumpPower = 0;
+
+                    currentLevel.addExit(
+                        new LevelExit(750, -350, placeholder, 300, 300)
+                    );
+
+                    break;
+                }
+                case 2: {
+                    currentLevel = new Scene(true, true, this);
+                    Texture placeholder = new Texture("cat.png");
+                    Texture instruction = new Texture("flagPlaceholder.png");
+                    Texture flagBase = new Texture("flagRed.png");
+
+                    addBasic();
+
+                    currentLevel.addGeneral(
+                        new Platform(220, 0, instruction, 800, 400)
+                    );
+
+                    Player player = currentLevel.addPlayer(
+                        new Player(-600, -500, placeholder, 400, 400)
+                    )[0];
+
+                    player.gravityStrength = 1.2f;
+                    player.jumpPower = 1.6f;
+
+                    currentLevel.addExit(
+                        new LevelExit(750, -350, placeholder, 300, 300)
+                    );
+
+                    currentLevel.addFlag(
+                        new Flag(-250, 140, flagBase, 300, 300)
+                    );
+
+
+                    break;
+                }
+                case 3: {
+                    currentLevel = new Scene(true, true, this);
+                    Texture placeholder = new Texture("cat.png");
+                    Texture instruction = new Texture("holdJumpPlaceholder.png");
+                    Texture flagBase = new Texture("flagRed.png");
+
+                    addBasic();
+
+                    currentLevel.addGeneral(
+                        new Platform(320, 0, instruction, 800, 400)
+                    );
+
+                    Player player = currentLevel.addPlayer(
+                        new Player(-600, -500, placeholder, 250, 250)
+                    )[0];
+
+                    currentLevel.addExit(
+                        new LevelExit(750, -400, placeholder, 200, 200)
+                    );
+
+                    currentLevel.addFlag(
+                        new Flag(-250, 80, flagBase, 200, 200)
+                    );
+
+
+                    break;
+                }
+                case 4: {
+                    currentLevel = new Scene(true, true, this);
+                    Texture placeholder = new Texture("cat.png");
+                    Texture instruction = new Texture("doubleJumpPlaceholder.png");
+                    Texture flagBase = new Texture("flagRed.png");
+
+                    addBasic();
+
+                    currentLevel.addGeneral(
+                        new Platform(320, 0, instruction, 800, 400)
+                    );
+
+                    Player player = currentLevel.addPlayer(
+                        new Player(-600, -500, placeholder, 250, 250)
+                    )[0];
+
+                    currentLevel.addExit(
+                        new LevelExit(750, -400, placeholder, 200, 200)
+                    );
+
+                    currentLevel.addFlag(
+                        new Flag(-250, 250, flagBase, 200, 200)
+                    );
+
+
+                    break;
+                }
+                default: {
+                    currentLevel = new Scene(true, true, this);
+                    Texture instruction = new Texture("exitPlaceholder.png");
+                    currentLevel.addGeneral(
+                        new Platform(220, 220, instruction, 800, 400),
+                        new Platform(-220, 220, instruction, 800, 400),
+                        new Platform(-220, -220, instruction, 800, 400),
+                        new Platform(220, -220, instruction, 800, 400)
+                    );
+                }
+            }
+        }
+    }
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        Texture image = new Texture("cat.png"); //Located in the assets file
 
-//        texture = new Texture(Gdx.files.internal("cat.png"));
-//        region = new TextureRegion(texture, 20, 20, 50, 50);
-//        sprite = new Sprite(texture, 20, 20, 50, 50);
-//        sprite.setPosition(100, 10);
-//        sprite.setColor(0, 0, 1, 1);
+        swapLevel(0);
 
-        currentLevel = new Scene();
 
-        currentLevel.addPlayer(
-            new Player(0, 0, image, 100, 100, true, 1)
-        );
-        //Adds to the scene's ArrayList of player objects.
-
-        currentLevel.addPlatform(
-            new Platform(0, 600, image, 150, 10).oscillateFromTo(-400,0,400f,70, 0.2f, 1),
-            new Platform(0, 600, image, 150, 50).oscillateRound(-400,0,1,2,4),
-            new Platform(250, -240, image, 450, 200)
-        );
-        //Adds to the scene's ArrayList of platform objects.
-        //One oscillates back and forth between two points, while the other revolves in a circle around a point.
-        //One is just static.
-
-        currentLevel.addGravity(
-            new GravityChange(100,200,image,50,50,false,0),
-            new GravityChange(-100,200,image,50,50,false,0)
-        );
 
         //currentLevel.objectsPlatform.get(0)
         //The scale of any GameplayObject image correlates directly to their width and height.
@@ -78,13 +254,22 @@ public class Main extends ApplicationAdapter {
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.75f, 0.15f, 0.2f, 1f); //Clears the screen by drawing the background each frame
+
+        switch(levelNumber) { //Different background clears for each level
+            case 0: {
+                ScreenUtils.clear(0f, 0f, 0f, 1f); //Clears the screen by drawing the background each frame
+                break;
+            }
+            default: {
+                ScreenUtils.clear(0.75f, 0.15f, 0.2f, 1f); //Clears the screen by drawing the background each frame
+            }
+        }
+
+
         batch.begin();
             currentLevel.tick();
             currentLevel.draw(batch);
         batch.end();
-
-        time++;
     }
 
     @Override
