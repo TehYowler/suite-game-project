4668406520119f309f86d7438e116edf74c02497
TehYowler <yowler@nmsu.edu>
2024-11-19 04:31:41 +0000

    Update Flag.java
    Added a "raise" method, and an optimal callback lambda function for whenever the flag is raised,

--- Project/core/src/main/java/com/rat/gamer/Flag.java
+++ Project/core/src/main/java/com/rat/gamer/Flag.java
@@ -3,8 +3,14 @@ package com.rat.gamer;
 import com.badlogic.gdx.graphics.Texture;
 
 public class Flag extends GameplayObject {
+    public AnyLambda onCollect = null;
     public boolean raised = false;
     public Flag(float x, float y, Texture image, int width, int height) {
         super(x, y, image, width, height);
+    }
+    public void raise() {
+        raised = true;
+        if(onCollect != null) onCollect.run();
+        image = new Texture("flagGreen.png");
     }
 }
