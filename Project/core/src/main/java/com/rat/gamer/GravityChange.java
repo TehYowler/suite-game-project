55453b2552a5d6ad9c155d8ab86172ff24a45dfd
TehYowler <yowler@nmsu.edu>
2024-11-19 04:32:17 +0000

    Update GravityChange.java
    Added an optional lambda function callback when the gravity pickup is picked up.

--- Project/core/src/main/java/com/rat/gamer/GravityChange.java
+++ Project/core/src/main/java/com/rat/gamer/GravityChange.java
@@ -4,16 +4,18 @@ import com.badlogic.gdx.graphics.Texture;
 
 public class GravityChange extends GameplayObject {
     private boolean inScene = true;
-    public GravityChange(float x, float y, Texture image, int width, int height, boolean useVelocity, float gravityStrength) {
-        super(x, y, image, width, height, useVelocity, gravityStrength);
-        tintRed = 0f;
-        tintGreen = 0f;
+    public AnyLambda onCollect = null;
+    public GravityChange(float x, float y, Texture image, int width, int height) {
+        super(x, y, image, width, height);
+        tintRed = 0.8f;
+        tintGreen = 0.8f;
     }
     public boolean exists() {
         return inScene;
     }
     public void remove() {
         inScene = false;
+        if(onCollect != null) onCollect.run();
     }
     public void tick(Scene scene) {
         if(!inScene) opacity = (float)Math.max(0,opacity - 0.02);
